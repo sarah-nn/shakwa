172ded704fa0549389727dc4e87d6bc84b34f3d7
ola2231 <oulamahfoud499@gmail.com>
2025-11-24 23:09:18 +0300

    handle error in custom text field

--- lib/Views/Widgets/custom_text_field.dart
+++ lib/Views/Widgets/custom_text_field.dart
@@ -11,14 +11,14 @@ class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
 
   const CustomTextField({
- 
     super.key,
     this.hint,
     this.icon,
     this.baseText,
     this.validator,
     this.isPassword = false,
-    required this.keyboardType, required this.controller,
+    required this.keyboardType,
+    required this.controller,
   });
 
   @override
@@ -27,7 +27,6 @@ class CustomTextField extends StatefulWidget {
 
 class _CustomTextFieldState extends State<CustomTextField> {
   bool _passVisible = false;
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,41 +45,22 @@ class _CustomTextFieldState extends State<CustomTextField> {
           const SizedBox(height: 10),
         ],
 
-        if (widget.baseText != null) ...[
-          Text(
-            widget.baseText!,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 10),
-        ],
-
         TextFormField(
           keyboardType: widget.keyboardType,
           obscureText: widget.isPassword ? !_passVisible : false,
           validator: widget.validator,
-
+          controller: widget.controller,
 
           decoration: InputDecoration(
             enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: AppColor.primaryColor,
-                width: 0.77,
-              ),
+              borderSide: BorderSide(color: AppColor.primaryColor, width: 0.77),
               borderRadius: BorderRadius.circular(7.69),
             ),
             focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: AppColor.primaryColor,
-                width: 0.77,
-              ),
-            
+              borderSide: BorderSide(color: AppColor.primaryColor, width: 0.77),
+
               borderRadius: BorderRadius.circular(7.69),
             ),
-
 
             hintText: widget.hint,
             hintStyle: const TextStyle(fontSize: 12, color: Colors.grey),
@@ -88,29 +68,25 @@ class _CustomTextFieldState extends State<CustomTextField> {
             filled: true,
             fillColor: AppColor.fillTextField,
 
-            prefixIcon: widget.icon != null
-                ? Icon(widget.icon, color: AppColor.primaryColor)
-                : null,
+            prefixIcon:
+                widget.icon != null
+                    ? Icon(widget.icon, color: AppColor.primaryColor)
+                    : null,
 
-        
-
-       
-
-            suffixIcon: widget.isPassword
-                ? InkWell(
-                    onTap: () {
-                      setState(() {
-                        _passVisible = !_passVisible;
-                      });
-                    },
-                    child: Icon(
-                      _passVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      color: AppColor.primaryColor,
-                    ),
-                  )
-                : null,
+            suffixIcon:
+                widget.isPassword
+                    ? InkWell(
+                      onTap: () {
+                        setState(() {
+                          _passVisible = !_passVisible;
+                        });
+                      },
+                      child: Icon(
+                        _passVisible ? Icons.visibility : Icons.visibility_off,
+                        color: AppColor.primaryColor,
+                      ),
+                    )
+                    : null,
 
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(7.69),
@@ -121,8 +97,6 @@ class _CustomTextFieldState extends State<CustomTextField> {
             ),
           ),
         ),
-
-        const SizedBox(height: 31),
 
         const SizedBox(height: 31),
       ],
